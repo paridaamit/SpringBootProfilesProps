dd500b5a69b807a2e30cab1777031e96489a711f
Amit <Amit@192.168.0.33>
2019-06-07 10:41:15 +0530

    again commit

--- SpringBootProfilesProps/src/main/java/com/app/runner/AppRunner.java
+++ SpringBootProfilesProps/src/main/java/com/app/runner/AppRunner.java
@@ -6,27 +6,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements CommandLineRunner {
-
 	@Value("${my.message}")
 	private String Message;
-
 	@Value("${my.code}")
 	private String code;
-
 	@Value("${my.mode}")
 	private String mode;
-	
 	@Override
 	public void run(String... args) throws Exception 
 	{
-		System.out.println(this);
-		
+		System.out.println(this);	
 	}
-
 	@Override
 	public String toString() {
 		return "AppRunner [Message=" + Message + ", code=" + code + ", mode=" + mode + "]";
-	}
-
-	
+	}	
 }
